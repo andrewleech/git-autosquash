19bc5516be5cd3257f8714819aadf8c4323efc38
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-08-30 15:31:14 +1000

    Clean up test file after alignment improvements

--- test_alignment.py
+++
@@ -1,4 +0,0 @@
-def test_alignment():
-    """Test function for checking alignment improvements."""
-    print("Testing improved visual alignment")
-    return "testing alignment and colors with better formatting"
